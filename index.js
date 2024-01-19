b4e7a47434e11ee799efb683faab1c8556560317
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-19 19:18:30 +0200

    hw_32 done

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+console.log("Сума: " + add(7, 4));
+console.log("Віднімання: " + subtract(7, 4));
+console.log("Множення: " + multiply(7, 4));
